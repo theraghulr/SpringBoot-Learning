03e61881967038f65f8b28381f40fad4d2c07254
theraghulr <rraghul1382@gmail.com>
2025-08-02 21:33:22 +0530

    learning API

---
+++ SpringBoot/src/main/java/com/springboot/learning/SpringBoot/Controller.java
@@ -0,0 +1,12 @@
+package com.springboot.learning.SpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Representational state transfer;(REST)
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    String Greeting(){
+        return "Hello mate, How are you?";
+    }
+}
